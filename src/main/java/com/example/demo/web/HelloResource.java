bf1e135891be71f12a0d1c7ee23f331e5de53fa2
p <p>
2019-10-02 07:42:14 +0530

    First Commit

---
+++ src/main/java/com/example/demo/web/HelloResource.java
@@ -0,0 +1,15 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+	@GetMapping("/sayHello")
+	public String sayHello(@RequestParam("name") String name) {
+		return "Hello, " + name;
+	}
+	
+}
